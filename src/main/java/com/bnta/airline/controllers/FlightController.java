203d4eaf8fe11d56a0df106e12eb51327bcada27
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-27 14:22:00 +0100

    added create new flight to controller

--- src/main/java/com/bnta/airline/controllers/FlightController.java
+++ src/main/java/com/bnta/airline/controllers/FlightController.java
@@ -1,14 +1,12 @@
 package com.bnta.airline.controllers;
 
 import com.bnta.airline.models.Flight;
+import com.bnta.airline.models.FlightDTO;
 import com.bnta.airline.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,13 +19,18 @@ public class FlightController {
 
     @GetMapping
     public ResponseEntity<List<Flight>> getAllFlights(){
-        List<Flight> flights = flightService.getAllFlights();
+        List<Flight> flights = flightService.findAllFlights();
         return new ResponseEntity<>(flights, HttpStatus.OK);
     }
     @GetMapping(value = "/{id}")
     public ResponseEntity<Flight> getFlightById(@PathVariable Long id){
         Flight foundFlight = flightService.findFlight(id);
         return new ResponseEntity(foundFlight, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<List<Flight>> postFlight(@RequestBody FlightDTO flightDTO){
+        flightService.saveFlight(flightDTO);
+        return new ResponseEntity<>(flightService.findAllFlights(), HttpStatus.CREATED);
     }
 
 
--- src/main/java/com/bnta/airline/services/FlightService.java
+++ src/main/java/com/bnta/airline/services/FlightService.java
@@ -1,6 +1,8 @@
 package com.bnta.airline.services;
 
 import com.bnta.airline.models.Flight;
+import com.bnta.airline.models.FlightDTO;
+import com.bnta.airline.models.Passenger;
 import com.bnta.airline.repositories.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,12 @@ public class FlightService {
     @Autowired
     FlightRepository flightRepository;
 
-    public Flight saveFlight(Flight flight){
-        flightRepository.save(flight);
-        return flight;
+    public Flight saveFlight(FlightDTO flightDTO){
+        Flight flight = new Flight(flightDTO.getDestination(),flightDTO.getCapacity(),flightDTO.getDepartureDate(),
+                flightDTO.getDepartureTime());
+        return flightRepository.save(flight);
     }
-    public List<Flight> getAllFlights() {
+    public List<Flight> findAllFlights() {
         return flightRepository.findAll();
     }
 
